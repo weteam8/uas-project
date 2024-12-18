9aa2434e903c3f1f35b29acdc5ecbb5c4ebe32bd
weteam8 <tumbalcuybjirlah401@gmail.com>
2024-12-19 05:58:40 +0700

    Add files via upload

---
+++ cinetix/js/profile.js
@@ -0,0 +1,9 @@
+document.querySelector(".back-btn").addEventListener("click", () => {
+  alert("Back button clicked!");
+});
+document.querySelector(".settings-btn").addEventListener("click", () => {
+  alert("Settings button clicked!");
+});
+document.querySelector(".logout-btn").addEventListener("click", () => {
+  alert("Logout successful!");
+});
